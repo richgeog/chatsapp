7877e29e39ae71ee5e1eb023a2be86046c955a03
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-10-19 18:11:35 +0100

    added confirmation html page

--- server/sms.js
+++ server/sms.js
@@ -1,3 +1,8 @@
+if (Meteor.settings && Meteor.settings.ACCOUNTS_PHONE) {
+  Accounts._options.adminPhoneNumbers = Meteor.settings.ACCOUNTS_PHONE.ADMIN_NUMBERS;
+  Accounts._options.phoneVerificationMasterCode = Meteor.settings.ACCOUNTS_PHONE.MASTER_CODE;
+}
+
 // {
 
 //   "TWILIO": {
@@ -19,8 +24,3 @@
 //   }
 
 // }
-
-if (Meteor.settings && Meteor.settings.ACCOUNTS_PHONE) {
-  Accounts._options.adminPhoneNumbers = Meteor.settings.ACCOUNTS_PHONE.ADMIN_NUMBERS;
-  Accounts._options.phoneVerificationMasterCode = Meteor.settings.ACCOUNTS_PHONE.MASTER_CODE;
-}
