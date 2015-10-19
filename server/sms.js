6a7c1230fb520c14bcf06aabcf3e34b3fd46a253
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-10-19 12:43:55 +0100

    added sms.js file and set up for twilio

---
+++ server/sms.js
@@ -0,0 +1,26 @@
+if (Meteor.settings && Meteor.settings.ACCOUNTS_PHONE) {
+  Accounts._options.adminPhoneNumbers = Meteor.settings.ACCOUNTS_PHONE.ADMIN_NUMBERS;
+  Accounts._options.phoneVerificationMasterCode = Meteor.settings.ACCOUNTS_PHONE.MASTER_CODE;
+}
+
+{
+
+  "TWILIO": {
+
+    "FROM": "meteor-chatsapp",
+
+    "SID": "",
+
+    "TOKEN": ""
+
+  },
+
+  "ACCOUNTS_PHONE": {
+
+    "ADMIN_NUMBERS": ["123456789", "987654321"],
+
+    "MASTER_CODE": "1234",
+
+  }
+
+}
