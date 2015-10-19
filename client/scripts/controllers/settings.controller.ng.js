1620719a1e8d443b1b5bcfbb67fe312cee0fbbf9
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-10-19 19:32:24 +0100

    settings controller logic added

---
+++ client/scripts/controllers/settings.controller.ng.js
@@ -0,0 +1,13 @@
+angular
+  .module('Chatsapp')
+  .controller('SettingsCtrl', SettingsCtrl);
+
+function SettingsCtrl($scope, $meteor, $state) {
+  $scope.logout = logout;
+
+  function logout() {
+    $meteor.logout().then(function () {
+      $state.go('login');
+    });
+  }
+}
