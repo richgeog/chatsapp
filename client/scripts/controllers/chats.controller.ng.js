1b185468b44e6c3ba432cf3bd70a1f85eb39a18c
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-10-20 10:56:00 +0100

    logic for new messages updated in chats controller

--- client/scripts/controllers/chats.controller.ng.js
+++ client/scripts/controllers/chats.controller.ng.js
@@ -2,9 +2,24 @@ angular
   .module('Chatsapp')
   .controller('ChatsCtrl', ChatsCtrl);
 
-  function ChatsCtrl ($scope) {
-    $scope.chats = $scope.$meteorCollection(Chats, false);
-    $scope.remove = remove;
+function ChatsCtrl ($scope, $ionicModal) {
+  $scope.chats = $scope.$meteorCollection(Chats, false);
+
+  $ionicModal.fromTemplateUrl('client/templates/new-chat.ng.html', { scope: $scope
+  }).then(function (modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.$on('$destroy', function () {
+    $scope.modal.remove();
+  });
+
+  $scope.openNewChatModal = openNewChatModal;
+  $scope.remove = remove;
+
+  function openNewChatModal () {
+    $scope.modal.show();
+  }
 
   function remove (chat) {
     $scope.chats.remove(chat);
